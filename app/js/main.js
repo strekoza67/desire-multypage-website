2e2c7be18b429d95a472e655fcbd607f4b678095
unknown <strekoza_nrv67@mail.ru>
2022-06-01 13:36:47 +0500

    header menu btns

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-$(function () {
+// $(function () {
+//   $('.header__btn').on('click', () => {
+//     $('.rightside-menu').removeClass('rightside-menu--close');
+//   });
+//   $('.rightside-menu__close').on('click', () => {
+//     $('.rightside-menu').addClass('rightside-menu--close');
+//   });
+// });
 
-})
+let menu = document.querySelector('.rightside-menu'),
+  menuBtn = document.querySelector('.header__btn'),
+  closeMenu = document.querySelector('.rightside-menu__close');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.remove('rightside-menu--close');
+});
+closeMenu.addEventListener('click', () => {
+  menu.classList.add('rightside-menu--close');
+});
