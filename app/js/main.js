58e918df26d743f69a7ad33b066f7ecf08922be8
unknown <strekoza_nrv67@mail.ru>
2022-06-05 19:40:11 +0500

    main-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,28 @@
-// $(function () {
-//   $('.header__btn').on('click', () => {
-//     $('.rightside-menu').removeClass('rightside-menu--close');
-//   });
-//   $('.rightside-menu__close').on('click', () => {
-//     $('.rightside-menu').addClass('rightside-menu--close');
-//   });
+$(function () {
+  $('.header__btn').on('click', () => {
+    $('.rightside-menu').removeClass('rightside-menu--close');
+  });
+  $('.rightside-menu__close').on('click', () => {
+    $('.rightside-menu').addClass('rightside-menu--close');
+  });
+
+  $('.top__slider').slick({
+    dots: true,
+    arrows: false
+  });
+});
+
+
+
+
+
+// let menu = document.querySelector('.rightside-menu'),
+//   menuBtn = document.querySelector('.header__btn'),
+//   closeMenu = document.querySelector('.rightside-menu__close');
+
+// menuBtn.addEventListener('click', () => {
+//   menu.classList.remove('rightside-menu--close');
 // });
-
-let menu = document.querySelector('.rightside-menu'),
-  menuBtn = document.querySelector('.header__btn'),
-  closeMenu = document.querySelector('.rightside-menu__close');
-
-menuBtn.addEventListener('click', () => {
-  menu.classList.remove('rightside-menu--close');
-});
-closeMenu.addEventListener('click', () => {
-  menu.classList.add('rightside-menu--close');
-});
+// closeMenu.addEventListener('click', () => {
+//   menu.classList.add('rightside-menu--close');
+// });
